9a22d490712576c4e7fd35812d7f87773b34fde4
michaeljaggers <michael.jaggers@gmail.com>
2021-06-17 21:14:45 -0500

    Added Solution

---
+++ SATProject.UI/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SATProject.UI.Startup))]
+namespace SATProject.UI
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
