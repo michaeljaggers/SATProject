92e439a7f0f71f55b1fe8947464d894b3828e568
michaeljaggers <michael.jaggers@gmail.com>
2021-06-24 18:19:27 -0500

    removing commented out code

--- SATProject.UI/Scripts/scripts.js
+++ SATProject.UI/Scripts/scripts.js
@@ -1,8 +1,4 @@
-﻿//$(document).ready(function () {
-//    $('#dataTable').DataTable();
-//});
-
-window.addEventListener('DOMContentLoaded', event => {
+﻿window.addEventListener('DOMContentLoaded', event => {
     // Simple-DataTables
     // https://github.com/fiduswriter/Simple-DataTables/wiki
 
